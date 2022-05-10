c41438743e4bbddbfe00a85cdd36c7b832fe6870
Plohotski <plohoc@gmail.com>
2022-05-10 19:54:04 +0300

    fix: add localstoredg

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ document.body.appendChild(component('textarea', '', 'textarea'));
 
 const inputText = document.querySelector('.textarea');
 let isInput = false;
-inputText.onclick = function isInp() { isInput = true; };
+inputText.addEventListener('click', () => { isInput = true; });
+document.addEventListener('mousedown', () => { isInput = false; });
 
 document.body.appendChild(component('div', '', 'keyboard'));
 document.body.appendChild(component('div', 'Switch language: leftCtrl + leftShift', 'text'));
@@ -28,7 +29,7 @@ const keyboard = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5',
 
 const outArr = ['Backspace', 'Win', 'Ctrl', 'Tab', 'Delete', 'CapsLock', 'Enter', 'Shift', 'Control', 'Meta', 'Alt', 'AltLeft', 'AltRight', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
-const textarea = [];
+let textarea = [];
 
 function init() {
   let out = '';
@@ -47,24 +48,28 @@ const allData = document.querySelectorAll('[data-name]');
 let isShift = false;
 let isCaps = false;
 
-document.onkeydown = function keyboardEvent(KeyboardEvent) {
+document.addEventListener('keydown', (KeyboardEvent) => {
   document.querySelector(`[data-name="${KeyboardEvent.code}"]`).classList.add('active');
-
   let tempKey = qwerty[lang][KeyboardEvent.code].key;
   if (isShift) tempKey = qwerty[lang][KeyboardEvent.code].shiftKey;
   if (isCaps) tempKey = qwerty[lang][KeyboardEvent.code].capsKey;
   switch (tempKey) {
     case 'Tab':
       textarea.push('\t');
-      if (isInput === false) inputText.value = textarea.join('');
+      if (isInput === false) inputText.value += '\t';
       break;
     case 'Enter':
-      textarea.push('\n\r');
-      if (isInput === false) inputText.value = textarea.join('');
+      textarea.push('\n');
+      if (isInput === false) inputText.value += '\n';
       break;
     case 'Backspace':
-      textarea.pop();
-      if (isInput === false) inputText.value = textarea.join('');
+      if (isInput === false) textarea.pop();
+      if (isInput === true) textarea = inputText.value.split('');
+      inputText.value = textarea.join('');
+      break;
+    case 'Delete':
+      textarea = inputText.value.split('');
+      inputText.value = textarea.join('');
       break;
     case 'CapsLock':
       if (isCaps === false) document.querySelector(`[data-name="${KeyboardEvent.code}"]`).classList.add('active');
@@ -74,91 +79,93 @@ document.onkeydown = function keyboardEvent(KeyboardEvent) {
       break;
   }
   if (outArr.includes(tempKey) !== true) {
-    if (isInput === false) inputText.value += tempKey;
-    textarea.push(tempKey);
+    if (!['▲', '◄', '▼', '◄'].includes(tempKey)) textarea.push(tempKey);
+    if (isInput === false) inputText.value = textarea.join('');
   }
-  setInterval(() => {
-    if (tempKey !== 'CapsLock') document.querySelector(`[data-name="${KeyboardEvent.code}"]`).classList.remove('active');
-  }, 500);
-};
+});
 
-document.querySelectorAll('.k-key').forEach((element) => {
-  element.addEventListener('click', function mouseEvent() {
-    document.querySelectorAll('.k-key').forEach((el) => {
-      if (el.dataset.name !== 'CapsLock' || el.dataset.name !== 'ShiftLeft' || el.dataset.name !== 'ShiftRight') el.classList.remove('active');
-    });
-    isInput = false;
-    const code = this.dataset.name;
-    this.classList.add('active');
+document.addEventListener('keyup', (KeyboardEvent) => {
+  if (!(KeyboardEvent.code === 'Capslock')) document.querySelector(`[data-name="${KeyboardEvent.code}"]`).classList.remove('active');
+});
 
-    let tempKey = qwerty[lang][code].key;
+function mouseEventKey() {
+  const code = this.dataset.name;
+  this.classList.add('active');
 
-    if (isShift) tempKey = qwerty[lang][code].shiftKey;
-    if (isCaps) tempKey = qwerty[lang][code].capsKey;
-    switch (tempKey) {
-      case 'Tab':
-        textarea.push('\t');
-        inputText.value = textarea.join('');
-        break;
-      case 'Enter':
-        textarea.push('\n\r');
-        inputText.value = textarea.join('');
-        break;
-      case 'Backspace':
-        textarea.pop();
-        inputText.value = textarea.join('');
-        break;
-      case 'Shift':
-        if (isShift === true) {
-          isShift = false;
-          this.classList.remove('active');
-          if (isCaps === false) {
-            for (let i = 0; i < allData.length; i += 1) {
-              const temp = allData[i].dataset.name;
-              allData[i].textContent = qwerty[lang][temp].key;
-            }
-          } else {
-            for (let i = 0; i < allData.length; i += 1) {
-              const temp = allData[i].dataset.name;
-              allData[i].textContent = qwerty[lang][temp].capsKey;
-            }
-          }
-        } else {
-          isShift = true;
-          for (let i = 0; i < allData.length; i += 1) {
-            const temp = allData[i].dataset.name;
-            allData[i].textContent = qwerty[lang][temp].shiftKey;
-          }
-          this.classList.add('active');
-        }
-        break;
-      case 'CapsLock':
-        if (isCaps === true) {
-          isCaps = false;
+  let tempKey = qwerty[lang][code].key;
+
+  if (isShift) tempKey = qwerty[lang][code].shiftKey;
+  if (isCaps) tempKey = qwerty[lang][code].capsKey;
+  switch (tempKey) {
+    case 'Tab':
+      textarea.push('\t');
+      inputText.value = textarea.join('');
+      break;
+    case 'Enter':
+      textarea.push('\n');
+      inputText.value = textarea.join('');
+      break;
+    case 'Backspace':
+      textarea.pop();
+      inputText.value = textarea.join('');
+      break;
+    case 'Shift':
+      if (isShift === true) {
+        isShift = false;
+        this.classList.remove('active');
+        if (isCaps === false) {
           for (let i = 0; i < allData.length; i += 1) {
             const temp = allData[i].dataset.name;
             allData[i].textContent = qwerty[lang][temp].key;
           }
-          this.classList.remove('active');
         } else {
-          isCaps = true;
           for (let i = 0; i < allData.length; i += 1) {
             const temp = allData[i].dataset.name;
             allData[i].textContent = qwerty[lang][temp].capsKey;
           }
-          this.classList.add('active');
         }
-        break;
-      default:
-        break;
-    }
-    if (outArr.includes(tempKey) !== true) {
-      inputText.value = textarea.join('');
-      textarea.push(tempKey);
-      setTimeout(() => {
-        if (tempKey !== 'CapsLock' || tempKey !== 'Shift') this.classList.remove('active');
-      }, 520);
-    }
+      } else {
+        isShift = true;
+        for (let i = 0; i < allData.length; i += 1) {
+          const temp = allData[i].dataset.name;
+          allData[i].textContent = qwerty[lang][temp].shiftKey;
+        }
+        this.classList.add('active');
+      }
+      break;
+    case 'CapsLock':
+      if (isCaps === true) {
+        isCaps = false;
+        for (let i = 0; i < allData.length; i += 1) {
+          const temp = allData[i].dataset.name;
+          allData[i].textContent = qwerty[lang][temp].key;
+        }
+        this.classList.remove('active');
+      } else {
+        isCaps = true;
+        for (let i = 0; i < allData.length; i += 1) {
+          const temp = allData[i].dataset.name;
+          allData[i].textContent = qwerty[lang][temp].capsKey;
+        }
+        this.classList.add('active');
+      }
+      break;
+    default:
+      break;
+  }
+  if (outArr.includes(tempKey) !== true) {
+    textarea.push(tempKey);
+    inputText.value = textarea.join('');
+  }
+}
+
+allData.forEach((element) => {
+  element.addEventListener('mousedown', mouseEventKey);
+});
+
+allData.forEach((key) => {
+  key.addEventListener('mouseup', (mouseEvent) => {
+    if (!['CapsLock', 'ShiftLeft', 'ShiftRight'].includes(mouseEvent.path[0].dataset.name)) document.querySelector(`[data-name="${mouseEvent.path[0].dataset.name}"]`).classList.remove('active');
   });
 });
 
@@ -175,7 +182,7 @@ document.addEventListener('keydown', (event) => {
     getTranslate();
   }
 
-  if (event.code === 'ShiftLeft') {
+  if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
     isShift = true;
     for (let i = 0; i < allData.length; i += 1) {
       const temp = allData[i].dataset.name;
@@ -201,7 +208,7 @@ document.addEventListener('keydown', (event) => {
 });
 
 document.addEventListener('keyup', (event) => {
-  if (event.code === 'ShiftLeft') {
+  if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
     isShift = false;
     if (isCaps === false) {
       for (let i = 0; i < allData.length; i += 1) {
@@ -211,8 +218,22 @@ document.addEventListener('keyup', (event) => {
     } else {
       for (let i = 0; i < allData.length; i += 1) {
         const temp = allData[i].dataset.name;
-        allData[i].textContent = qwerty[lang][temp].capsKey;
+        allData[i].textContent = qwerty[lang][temp].shiftKey;
       }
     }
   }
 });
+
+function setLocalStorage() {
+  localStorage.setItem('globalLang', lang);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+  if (localStorage.getItem('globalLang')) {
+    const globalLang = localStorage.getItem('globalLang');
+    lang = globalLang;
+    getTranslate(globalLang);
+  }
+}
+window.addEventListener('load', getLocalStorage);
